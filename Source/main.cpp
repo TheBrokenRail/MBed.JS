0a66b11b8eafd5bb527df052df77df959ecd1519
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-13 10:15:56 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,6 +2,16 @@
 #include "duktape/duktape.h"
 #include "JSSource.h"
 
+static duk_ret_t native_print(duk_context *ctx) {
+  printf("%s\n", duk_to_string(ctx, 0));
+  return 0;  /* no return value (= undefined) */
+}
+
 int main() {
-  
+  duk_context *ctx = duk_create_heap_default();
+  duk_push_c_function(ctx, native_print, 1);
+  duk_put_global_string(ctx, "printf");
+  duk_eval_string(ctx, jsSource);
+  duk_destroy_heap(ctx);
+  return 0;
 }
