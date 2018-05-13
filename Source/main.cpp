a8d141e63ecf30a093b88ecde875a4b227663751
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-13 11:37:15 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,8 +2,10 @@
 #include "duktape/duktape.h"
 #include "JSSource.h"
 
+Serial pc(USBTX, USBRX);
+
 static duk_ret_t native_print(duk_context *ctx) {
-  printf("%s\n", duk_to_string(ctx, 0));
+  pc.printf("%s\n", duk_to_string(ctx, 0));
   return 0;  /* no return value (= undefined) */
 }
 
